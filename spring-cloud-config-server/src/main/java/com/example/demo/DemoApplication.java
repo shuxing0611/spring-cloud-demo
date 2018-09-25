9a95e1a908b46208b9e5964eba0a680bfdb6ce4d
shuxing0611 <250494669@qq.com>
2018-09-25 18:28:41 +0800

    通用配置

--- spring-cloud-config-server/src/main/java/com/example/demo/DemoApplication.java
+++ spring-cloud-config-server/src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @SpringBootApplication
+@RestController
 @EnableConfigServer
-@EnableEurekaClient
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
 }
